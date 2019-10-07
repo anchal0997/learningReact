8625e8f602d0a3569d654c9fe2696df4f3816825
agarg5 <Anchal_Garg@intuit.com>
2019-10-07 19:22:15 +0530

    Set states

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -2,17 +2,34 @@ import React,{Component} from 'react';
 
 
 class App extends Component{
+
+    constructor(){
+        super();
+        this.state={displayBio:false};
+        this.readMore=this.readMore.bind(this);
+    }
+
+    readMore(){
+        this.setState({displayBio:true})
+    }
     render(){
+        let bio =(<div>
+            <h1>ANOTHER PARA</h1>
+        <p>I love MUSIC</p>
+    </div>);
+    if(!this.state.displayBio)
+    {
+        bio=(<div>HELLO</div>);
+    }
         return(
             <div>
                 <h1>ANCHAL</h1>
                 <p>
                     I am a girl !!!!!
+
+                    <button onClick={this.readMore}>Read More</button>
                 </p>
-                <div>
-                    <h1>ANOTHER PARA</h1>
-                <p>I love MUSIC</p>
-            </div>
+                {bio}
             </div>
             
         )
